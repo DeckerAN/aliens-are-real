54619510bfd6e45e5151cbaddf8451a931e3d6df
Decker <45881871+DeckerAN@users.noreply.github.com>
2019-05-21 18:51:39 -0700

    finished part 1

--- static/js/app.js
+++ static/js/app.js
@@ -17,6 +17,49 @@ tableData.forEach(function(collection) {
     row.append("td").text(collection.comments);
 });
 
+// Filtering Table by Date using Forms
+var dateFilter = d3.select('#filter-btn');
+
+dateFilter.on('click', function() {
+    
+    d3.event.preventDefault();
+    
+    d3.select("tbody").text("");
+    
+    var inputElement = d3.select("#datetime");
+    
+    var inputValue = inputElement.property("value");
+
+    tableData.forEach(function(collection) {
+        if (inputValue === collection.datetime) {
+            var row = tbody.append("tr");
+            row.append("td").text(collection.datetime);
+            row.append("td").text(collection.city);
+            row.append("td").text(collection.state);
+            row.append("td").text(collection.country);
+            row.append("td").text(collection.shape);
+            row.append("td").text(collection.durationMinutes);
+            row.append("td").text(collection.comments);
+        }
+        else if (inputValue === "" ) {
+            var row = tbody.append("tr");
+            row.append("td").text(collection.datetime);
+            row.append("td").text(collection.city);
+            row.append("td").text(collection.state);
+            row.append("td").text(collection.country);
+            row.append("td").text(collection.shape);
+            row.append("td").text(collection.durationMinutes);
+            row.append("td").text(collection.comments);
+        }
+    });
+    
+    
+    
+    
+    
+    
+});
+
 
 
 
